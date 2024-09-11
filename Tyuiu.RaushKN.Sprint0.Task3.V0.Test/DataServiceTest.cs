14aefb84e5aaabcea25ac117467df8e52cff609d
Кирилл Рауш <Tyuiu.RaushKN@outlook.com>
2024-09-12 00:36:17 +0500

    Task2

---
+++ Tyuiu.RaushKN.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+using Tyuiu.RaushKN.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.RaushKN.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
